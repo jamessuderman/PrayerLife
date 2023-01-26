abb14ddf7fd696c6309219176a1a1c093c9b1982
James Suderman <jamessuderman@gmail.com>
2023-01-25 20:44:35 -0600

    Update Request.cs

--- Models/Request.cs
+++ Models/Request.cs
@@ -5,5 +5,6 @@ public class Request
     [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
     public string Body { get; set; }
+    public DateTime DateInserted { get; set; } = DateTime.Now;
     public bool IsAnswered { get; set; } = false;
 }
